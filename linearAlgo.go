db817c320b19c472235e07e2dbf6fb20d350c589
abskydev <Ghjpl125711pl>
2021-09-26 15:48:27 +0200

    End working with linear algorithm

--- linearAlgo.go
+++ linearAlgo.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func linearEquation(b float64, c float64) {
-	fmt.Println(b, c)
+	if b == 0 {
+		if c == 0 {
+			fmt.Println("identity equation")
+		} else {
+			fmt.Println("contradictory equation")
+		}
+	} else {
+		var x = -c / b
+		fmt.Println("x = ", x)
+	}
 }
